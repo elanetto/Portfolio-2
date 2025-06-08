543aea83042fddd3d62f0dee82885f6199e6c0a3
elanetto <101054274+elanetto@users.noreply.github.com>
2025-06-09 00:27:50 +0200

    Fixed typo

--- src/Pages/Portfolio/eShop/index.jsx
+++ src/Pages/Portfolio/eShop/index.jsx
@@ -9,7 +9,7 @@ function Nettbutikk() {
     <PortfolioLayout
       title="eShop"
       description="Dette prosjektet er en fiktiv nettbutikk utviklet med React, Vite, Tailwind og vanilla JavaScript, basert på en gitt API med 25 produkter. Oppgaven gikk ut på å sette opp en fullverdig frontend-løsning med handlekurv og valideringslogikk."
-      reflections="Dette prosjektet er det første react prosjektet jeg har gjort med en gitt API. Det viktigte med dette prosjektet var å få koden til å fungere: Altså at nettsiden skulle fungere i forhold til søking og kjøping av de 25 tilgjengelige produktene. I ettertid er mitt største forbedringspotensial i dette prosjektet designen av nettsiden. Blåfargen får siden til å se halvferdig ut."
+      reflections="Dette prosjektet er det første react prosjektet jeg har gjort med en gitt API. Det viktigste med dette prosjektet var å få koden til å fungere: Altså at nettsiden skulle fungere i forhold til søking og kjøping av de 25 tilgjengelige produktene. I ettertid er mitt største forbedringspotensial i dette prosjektet designen av nettsiden. Blåfargen får siden til å se halvferdig ut."
       images={[eShopImg1, eShopImg2, eShopImg3]}
       icons={[
         TechIcons.react,
